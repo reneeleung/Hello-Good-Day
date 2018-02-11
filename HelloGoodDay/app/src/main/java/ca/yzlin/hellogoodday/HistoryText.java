840fa3338efeaf5e7ebb1c148a83fb0456cb44c5
KarolinaXin <karolinaxin@yahoo.ca>
2018-02-11 03:46:13 -0500

    finished save and history

--- HelloGoodDay/app/src/main/java/ca/yzlin/hellogoodday/HistoryText.java
+++ HelloGoodDay/app/src/main/java/ca/yzlin/hellogoodday/HistoryText.java
@@ -6,19 +6,25 @@ import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class HistoryText extends AppCompatActivity {
     private SharedPreferences props;
+    private SharedPreferences descrip;
+    private ArrayList<HGDEntry> entries = new ArrayList<HGDEntry>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history_text);
         props = getSharedPreferences(MainActivity.SAVED_PROPS_FILE, 0);
+        descrip = getSharedPreferences(MainActivity.SAVED_DESCRIP_FILE, 0);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.linear_layout);
         LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         int size = props.getInt("indexAt", -1);
-        if(size==0){
+        if(size==-1 || size==0){
             TextView tv = new TextView(this);
             tv.setLayoutParams(lparams);
             tv.setText("Nothing's been saved yet!");
@@ -26,7 +32,22 @@ public class HistoryText extends AppCompatActivity {
         }
         else{
             for(int i=0; i<(size+1); i++){
-
+                String fields = props.getString(String.valueOf(i), "");
+                if(fields=="") continue;
+                String[] arg = fields.split(" ");
+                if(arg.length < 3) continue;
+                String longi = "0";
+                String lati = "0";
+                if(arg.length>=4) longi = arg[3];
+                if(arg.length>=5) lati = arg[4];
+                String descripId = arg[1];
+                String text = descrip.getString(descripId,"");
+                HGDEntry ent = new HGDEntry(arg[0], text, arg[2], longi, lati);
+                entries.add(ent);
+                TextView tv = new TextView(this);
+                tv.setLayoutParams(lparams);
+                tv.setText(ent.getText());
+                layout.addView(tv);
             }
         }
     }
